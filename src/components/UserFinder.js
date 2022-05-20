2c8924b943902f0c34952a8d3672d7d82260fcda
ben <spartanhooah@gmail.com>
2022-05-20 13:09:16 -0400

    add error boundary

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -3,6 +3,7 @@ import { Component } from "react";
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
 import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary"
 
 class UserFinder extends Component {
   // How to use context in a class-based component
@@ -40,7 +41,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </>
     );
   }
